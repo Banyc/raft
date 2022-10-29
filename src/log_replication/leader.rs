9cf9321fedb430c02321bada0446abe3f473acf0
Banyc <36535895+Banyc@users.noreply.github.com>
2022-10-30 01:25:57 +0800

    docs: leader

--- src/log_replication/leader.rs
+++ src/log_replication/leader.rs
@@ -24,8 +24,13 @@ impl Leader {
         Leader { log, follower_logs }
     }
 
+    /// - Goal: to compliment what log entries the follower lacks
     pub fn emit(&self, to: Node) -> Result<AppendEntriesReq, EmitError> {
         let follower_log = self.follower_logs.get(&to).ok_or(EmitError::UnknownNode)?;
+
+        // Get the log index of the entry that:
+        // - the follower probably has
+        // - the last entry of the follower so that we can just send few entries
         let prev_entry = if follower_log.next_index == 0 {
             None
         } else {
@@ -36,11 +41,15 @@ impl Leader {
                 term: prev_term,
             })
         };
+
+        // Get the entries that the follower lacks
         let new_entries = self
             .log
             .entries_from(follower_log.next_index)
             .map(|v| *v)
             .collect::<Vec<_>>();
+
+        // Bring in the commit index to permit the follower to commit entries
         Ok(AppendEntriesReq {
             new_entries,
             prev_entry,
@@ -48,6 +57,9 @@ impl Leader {
         })
     }
 
+    /// - Goal: a feedback to adjust:
+    ///   - the entry commit when some entries have replicated in the majority of nodes
+    ///   - what entries the followers lack
     pub fn append_entries_resp(
         &mut self,
         term: Term,
@@ -58,6 +70,8 @@ impl Leader {
             .follower_logs
             .get_mut(&from)
             .ok_or(AppendEntriesError::UnknownNode)?;
+
+        // Adjust the information of what entries the follower lacks
         match res {
             AppendEntriesRes::Success { match_index } => {
                 if match_index >= self.log.len() {
@@ -69,37 +83,48 @@ impl Leader {
                     }
                 }
 
+                // Mark all the entries up to the match index as replicated
                 follower_log.match_index = Some(match_index);
+
+                // To send the rest of the entries in the future
                 follower_log.next_index = match_index + 1;
             }
             AppendEntriesRes::Failure { new_next_index } => {
                 if new_next_index >= self.log.len() {
                     return Err(AppendEntriesError::InvalidNextIndex);
                 }
-                if follower_log.next_index < new_next_index {
+                if follower_log.next_index <= new_next_index {
                     return Err(AppendEntriesError::NextIndexTooLarge);
                 }
 
+                // The follower lacks even more entries than anticipated before
+                // To send more old entries in the future
                 follower_log.next_index = new_next_index;
 
                 return Ok(());
             }
         }
 
-        // commit
+        // Commit entries that have replicated in the majority of nodes
         if let Some(&last_term) = self.log.uncommitted().back() {
             if last_term != term {
                 // a leader cannot determine commitment using log entries from older terms
                 return Ok(());
             }
+
             let mut match_indices = self
                 .follower_logs
                 .values()
                 .filter_map(|follower_log| follower_log.match_index)
                 .collect::<Vec<_>>();
+
             match_indices.sort_unstable();
+
             let commit_index = match_indices[match_indices.len() / 2];
+
             let success = self.log.try_commit(commit_index);
+
+            // SAFETY: none of the `match_indices` can go beyond the length of the log
             assert!(success);
         }
         Ok(())
